795e543b108c237a2894be01d3c7c1ce3dc1808e
Ojas Ahirrao <ojasahirrao530@gmail.com>
2021-09-30 21:08:51 +0530

    added diff.cpp file

---
+++ diff.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int x, y, sub;
+
+    cout << "Enter two numbers: ";
+    cin >> x >> y;
+
+    sub = x - y;
+
+    cout << "Subtraction of two numbers is: " << sub;
+
+    return 0;
+}
